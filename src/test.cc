e919ac70ac06f2202e18f87352aec48d6c88c0fc
Yang <yang@yang-cs.com>
2013-02-23 10:54:42 -0500

    updated to A2-2

--- src/test.cc
+++ src/test.cc
@@ -1,163 +1,161 @@
 #include "test.h"
 #include "BigQ.h"
 #include <pthread.h>
+void test1 ();
+void test2 ();
+void test3 ();
 
-//#define CISE
-
-#ifndef CISE     // local machine
-#define DBFILE_DIR "../DATA/"
-#define TPCH_DIR "../data/"
-#define CATALOG_PATH "catalog"
-#else            // department machine
-#define DBFILE_DIR "../DATA/"
-#define TPCH_DIR "../DATA/"
-#define CATALOG_PATH "catalog"
-#endif // CISE
-
-void *producer (void *arg) {
-	Pipe *myPipe = (Pipe *) arg;
-
-	Record temp;
-	int counter = 0;
-
+int add_data (FILE *src, int numrecs, int &res) {
 	DBFile dbfile;
 	dbfile.Open (rel->path ());
-	cout << " producer: opened DBFile " << rel->path () << endl;
-	dbfile.MoveFirst ();
+	Record temp;
 
-	while (dbfile.GetNext (temp) == 1) {
-		counter += 1;
-		if (counter%10000 == 0) {
-			 cerr << " producer: " << counter << endl;	
-		}
-		myPipe->Insert (&temp);
+	int proc = 0;
+	int xx = 20000;
+	while ((res = temp.SuckNextRecord (rel->schema (), src)) && ++proc < numrecs) {
+		dbfile.Add (temp);
+		if (proc == xx) cerr << "\t ";
+		if (proc % xx == 0) cerr << ".";
 	}
 
 	dbfile.Close ();
-	myPipe->ShutDown ();
-
-	cout << " producer: inserted " << counter << " recs into the pipe\n";
+	return proc;
 }
 
-void *consumer (void *arg) {
-	
-	testutil *t = (testutil *) arg;
 
-	ComparisonEngine ceng;
+// create a dbfile interactively
+void test1 () {
+
+
+	OrderMaker o;
+	rel->get_sort_order (o);
+
+	int runlen = 0;
+	while (runlen < 1) {
+		cout << "\t\n specify runlength:\n\t ";
+		cin >> runlen;
+	}
+	struct {OrderMaker *o; int l;} startup = {&o, runlen};
 
 	DBFile dbfile;
-	char outfile[100];
+	cout << "\n output to dbfile : " << rel->path () << endl;
+	dbfile.Create (rel->path(), sorted, &startup);
+	dbfile.Close ();
 
-	if (t->write) {
-		sprintf (outfile, "%s.bigq", rel->path ());
-		dbfile.Create (outfile, heap, NULL);
-	}
+	char tbl_path[100];
+	sprintf (tbl_path, "%s%s.tbl", tpch_dir, rel->name()); 
+	cout << " input from file : " << tbl_path << endl;
 
-	int err = 0;
-	int i = 0;
+        FILE *tblfile = fopen (tbl_path, "r");
 
-	Record rec[2];
-	Record *last = NULL, *prev = NULL;
+	srand48 (time (NULL));
 
-	while (t->pipe->Remove (&rec[i%2])) {
-		prev = last;
-		last = &rec[i%2];
-
-		if (prev && last) {
-			if (ceng.Compare (prev, last, t->order) == 1) {
-				err++;
-			}
-			if (t->write) {
-				dbfile.Add (*prev);
-			}
+	int proc = 1, res = 1, tot = 0;
+	while (proc && res) {
+		int x = 0;
+		while (x < 1 || x > 3) {
+			cout << "\n select option for : " << rel->path () << endl;
+			cout << " \t 1. add a few (1 to 1k recs)\n";
+			cout << " \t 2. add a lot (1k to 1e+06 recs) \n";
+			cout << " \t 3. run some query \n \t ";
+			cin >> x;
 		}
-		if (t->print) {
-			last->Print (rel->schema ());
+		if (x < 3) {
+			proc = add_data (tblfile,lrand48()%(int)pow(1e3,x)+(x-1)*1000, res);
+			tot += proc;
+			if (proc) 
+				cout << "\n\t added " << proc << " recs..so far " << tot << endl;
 		}
-		i++;
-	}
-
-	cout << " consumer: removed " << i << " recs from the pipe\n";
-
-	if (t->write) {
-		if (last) {
-			dbfile.Add (*last);
+		else {
+			test3 ();
 		}
-		cerr << " consumer: recs removed written out as heap DBFile at " << outfile << endl;
-		dbfile.Close ();
 	}
-	cerr << " consumer: " << (i - err) << " recs out of " << i << " recs in sorted order \n";
-	if (err) {
-		cerr << " consumer: " <<  err << " recs failed sorted order test \n" << endl;
-	}
+	cout << "\n create finished.. " << tot << " recs inserted\n";
+	fclose (tblfile);
 }
 
+// sequential scan of a DBfile 
+void test2 () {
 
-void test1 (int option, int runlen) {
+	cout << " scan : " << rel->path() << "\n";
+	DBFile dbfile;
+	dbfile.Open (rel->path());
+	dbfile.MoveFirst ();
 
-	// sort order for records
-	OrderMaker sortorder;
-	rel->get_sort_order (sortorder);
+	Record temp;
 
-	int buffsz = 100; // pipe cache size
-	Pipe input (buffsz);
-	Pipe output (buffsz);
-
-	// thread to dump data into the input pipe (for BigQ's consumption)
-	pthread_t thread1;
-	pthread_create (&thread1, NULL, producer, (void *)&input);
-
-	// thread to read sorted data from output pipe (dumped by BigQ)
-	pthread_t thread2;
-	testutil tutil = {&output, &sortorder, false, false};
-	if (option == 2) {
-		tutil.print = true;
+	int cnt = 0;
+	cerr << "\t";
+	while (dbfile.GetNext (temp) && ++cnt) {
+		temp.Print (rel->schema());
+		if (cnt % 10000) {
+			cerr << ".";
+		}
 	}
-	else if (option == 3) {
-		tutil.write = true;
+	cout << "\n scanned " << cnt << " recs \n";
+	dbfile.Close ();
+}
+
+void test3 () {
+
+	CNF cnf; 
+	Record literal;
+	rel->get_cnf (cnf, literal);
+
+	DBFile dbfile;
+	dbfile.Open (rel->path());
+	dbfile.MoveFirst ();
+
+	Record temp;
+
+	int cnt = 0;
+	cerr << "\t";
+	while (dbfile.GetNext (temp, cnf, literal) && ++cnt) {
+		temp.Print (rel->schema());
+		if (cnt % 10000 == 0) {
+			cerr << ".";
+		}
 	}
-	pthread_create (&thread2, NULL, consumer, (void *)&tutil);
+	cout << "\n query over " << rel->path () << " returned " << cnt << " recs\n";
+	dbfile.Close ();
 
-	BigQ bq (input, output, sortorder, runlen);
-
-	pthread_join (thread1, NULL);
-	pthread_join (thread2, NULL);
 }
 
 int main (int argc, char *argv[]) {
 
 	setup ();
 
-	relation *rel_ptr[] = {n, r, c, p, ps, o, li};
+	relation *rel_ptr[] = {n, r, c, p, ps, s, o, li};
+	void (*test_ptr[]) () = {&test1, &test2, &test3};  
+	void (*test) ();
 
 	int tindx = 0;
 	while (tindx < 1 || tindx > 3) {
 		cout << " select test option: \n";
-		cout << " \t 1. sort \n";
-		cout << " \t 2. sort + display \n";
-		cout << " \t 3. sort + write \n\t ";
+		cout << " \t 1. create sorted dbfile\n";
+		cout << " \t 2. scan a dbfile\n";
+		cout << " \t 3. run some query \n \t ";
 		cin >> tindx;
 	}
 
 	int findx = 0;
-	while (findx < 1 || findx > 7) {
-		cout << "\n select dbfile to use: \n";
+	while (findx < 1 || findx > 8) {
+		cout << "\n select table: \n";
 		cout << "\t 1. nation \n";
 		cout << "\t 2. region \n";
 		cout << "\t 3. customer \n";
 		cout << "\t 4. part \n";
 		cout << "\t 5. partsupp \n";
-		cout << "\t 6. orders \n";
-		cout << "\t 7. lineitem \n \t ";
+		cout << "\t 6. supplier \n";
+		cout << "\t 7. orders \n";
+		cout << "\t 8. lineitem \n \t ";
 		cin >> findx;
 	}
 	rel = rel_ptr [findx - 1];
 
-	int runlen;
-	cout << "\t\n specify runlength:\n\t ";
-	cin >> runlen;
-	
-	test1 (tindx, runlen);
+	test = test_ptr [tindx-1];
+	test ();
 
 	cleanup ();
+	cout << "\n\n";
 }
--- src/test.h
+++ src/test.h
@@ -1,19 +1,20 @@
 #ifndef TEST_H
 #define TEST_H
 #include <stdio.h>
-#include <iostream>
 #include <stdlib.h>
+#include <iostream>
+#include <math.h>
+
 #include "Pipe.h"
 #include "DBFile.h"
 #include "Record.h"
-
 using namespace std;
 
 // make sure that the information below is correct
 
 char *catalog_path = "catalog"; 
-char *tpch_dir ="../DATA/"; // dir where dbgen tpch files (extension *.tbl) can be found
-char *dbfile_dir = "../DATA/"; 
+char *dbfile_dir = ""; 
+char *tpch_dir ="/cise/tmp/dbi_sp11/DATA/1G/"; 
 
 
 extern "C" {
@@ -51,9 +52,9 @@ public:
 	}
 
 	void get_cnf (CNF &cnf_pred, Record &literal) {
-		cout << " Enter CNF predicate (when done press ctrl-D):\n\t";
+		cout << "\n enter CNF predicate (when done press ctrl-D):\n\t";
   		if (yyparse() != 0) {
-			cout << "Can't parse your CNF.\n";
+			cout << " Error: can't parse your CNF.\n";
 			exit (1);
 		}
 		cnf_pred.GrowFromParseTree (final, schema (), literal); // constructs CNF predicate
@@ -61,10 +62,9 @@ public:
 	void get_sort_order (OrderMaker &sortorder) {
 		cout << "\n specify sort ordering (when done press ctrl-D):\n\t ";
   		if (yyparse() != 0) {
-			cout << "Can't parse your sort CNF.\n";
+			cout << " Error: can't parse your CNF.\n";
 			exit (1);
 		}
-		cout << " \n";
 		Record literal;
 		CNF sort_pred;
 		sort_pred.GrowFromParseTree (final, schema (), literal); // constructs CNF predicate
