d9898700d82bd05268c3a5271fb4ed05f6a01448
Yang <yang@yang-cs.com>
2013-03-12 22:21:06 -0400

    fixed a test bug in assignment 2

--- src/test.cc
+++ src/test.cc
@@ -14,7 +14,8 @@ int add_data (FILE *src, int numrecs, int &res) {
 
 	int proc = 0;
 	int xx = 20000;
-	while ((res = temp.SuckNextRecord (rel->schema (), src)) && ++proc < numrecs) {
+	while ((proc < numrecs) && (res = temp.SuckNextRecord (rel->schema (), src))) {
+          ++proc;
 		dbfile.Add (temp);
 		if (proc == xx) cerr << "\t ";
 		if (proc % xx == 0) cerr << ".";
