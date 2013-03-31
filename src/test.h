fdaca7c2aaba067cbd3cdc4ab830f619c35830ec
Yang <yang@yang-cs.com>
2013-03-31 12:07:47 -0400

    Created an `a1' branch for loading heap files.
    I created an a1 branch for loading heap files. In addition, I added
    read/write mode to heap files.

--- src/test.h
+++ src/test.h
@@ -3,41 +3,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <iostream>
-#include <math.h>
-
-#include "Function.h"
-#include "Pipe.h"
 #include "DBFile.h"
 #include "Record.h"
-
 using namespace std;
-
-// test settings file should have the 
-// catalog_path, dbfile_dir and tpch_dir information in separate lines
-const char *settings = "test.cat";
-
-// donot change this information here
-char *catalog_path, *dbfile_dir, *tpch_dir = NULL;
 
 extern "C" {
 	int yyparse(void);   // defined in y.tab.c
-	int yyfuncparse(void);   // defined in yyfunc.tab.c
-	void init_lexical_parser (char *); // defined in lex.yy.c (from Lexer.l)
-	void close_lexical_parser (); // defined in lex.yy.c
-	void init_lexical_parser_func (char *); // defined in lex.yyfunc.c (from Lexerfunc.l)
-	void close_lexical_parser_func (); // defined in lex.yyfunc.c
 }
 
 extern struct AndList *final;
-extern struct FuncOperator *finalfunc;
-extern FILE *yyin;
-
-typedef struct {
-	Pipe *pipe;
-	OrderMaker *order;
-	bool print;
-	bool write;
-}testutil;
 
 class relation {
 
@@ -60,96 +34,15 @@ public:
 		cout << "\t path: " << path () << endl;
 	}
 
-	void get_cnf (char *input, CNF &cnf_pred, Record &literal) {
-		init_lexical_parser (input);
-  		if (yyparse() != 0) {
-			cout << " Error: can't parse your CNF.\n";
-			exit (1);
-		}
-		cnf_pred.GrowFromParseTree (final, schema (), literal); // constructs CNF predicate
-		close_lexical_parser ();
-	}
-
-	void get_cnf (char *input, Function &fn_pred) {
-		init_lexical_parser_func (input);
-  		if (yyfuncparse() != 0) {
-			cout << " Error: can't parse your CNF.\n";
-			exit (1);
-		}
-		fn_pred.GrowFromParseTree (finalfunc, *(schema ())); // constructs CNF predicate
-		close_lexical_parser_func ();
-	}
-
 	void get_cnf (CNF &cnf_pred, Record &literal) {
-		cout << "\n enter CNF predicate (when done press ctrl-D):\n\t";
+		cout << " Enter CNF predicate (when done press ctrl-D):\n\t";
   		if (yyparse() != 0) {
-			cout << " Error: can't parse your CNF.\n";
+			std::cout << "Can't parse your CNF.\n";
 			exit (1);
 		}
 		cnf_pred.GrowFromParseTree (final, schema (), literal); // constructs CNF predicate
-	}
-
-	void get_file_cnf (const char *fpath, CNF &cnf_pred, Record &literal) {
-		yyin = fopen (fpath, "r");
-  		if (yyin == NULL) {
-			cout << " Error: can't open file " << fpath << " for parsing \n";
-			exit (1);
-		}
-		if (yyparse() != 0) {
-			cout << " Error: can't parse your CNF.\n";
-			exit (1);
-		}
-		cnf_pred.GrowFromParseTree (final, schema (), literal); // constructs CNF predicate
-		// cnf_pred.GrowFromParseTree (final, l_schema (), r_schema (), literal); // constructs CNF predicate over two relations l_schema is the left reln's schema r the right's
-		//cnf_pred.Print ();
-	}
-
-
-	void get_sort_order (OrderMaker &sortorder) {
-		cout << "\n specify sort ordering (when done press ctrl-D):\n\t ";
-  		if (yyparse() != 0) {
-			cout << " Error: can't parse your CNF \n";
-			exit (1);
-		}
-		Record literal;
-		CNF sort_pred;
-		sort_pred.GrowFromParseTree (final, schema (), literal); // constructs CNF predicate
-		OrderMaker dummy;
-		sort_pred.GetSortOrders (sortorder, dummy);
 	}
 };
-
-void get_cnf (char *input, Schema *left, CNF &cnf_pred, Record &literal) {
-	init_lexical_parser (input);
-  	if (yyparse() != 0) {
-		cout << " Error: can't parse your CNF " << input << endl;
-		exit (1);
-	}
-	cnf_pred.GrowFromParseTree (final, left, literal); // constructs CNF predicate
-	close_lexical_parser ();
-}
-
-void get_cnf (char *input, Schema *left, Schema *right, CNF &cnf_pred, Record &literal) {
-	init_lexical_parser (input);
-  	if (yyparse() != 0) {
-		cout << " Error: can't parse your CNF " << input << endl;
-		exit (1);
-	}
-	cnf_pred.GrowFromParseTree (final, left, right, literal); // constructs CNF predicate
-	close_lexical_parser ();
-}
-
-void get_cnf (char *input, Schema *left, Function &fn_pred) {
-		init_lexical_parser_func (input);
-  		if (yyfuncparse() != 0) {
-			cout << " Error: can't parse your arithmetic expr. " << input << endl;
-			exit (1);
-		}
-		fn_pred.GrowFromParseTree (finalfunc, *left); // constructs CNF predicate
-		close_lexical_parser_func ();
-}
-
-relation *rel;
 
 char *supplier = "supplier"; 
 char *partsupp = "partsupp"; 
@@ -162,31 +55,7 @@ char *lineitem = "lineitem";
 
 relation *s, *p, *ps, *n, *li, *r, *o, *c;
 
-void setup () {
-	FILE *fp = fopen (settings, "r");
-	if (fp) {
-		char *mem = (char *) malloc (80 * 3);
-		catalog_path = &mem[0];
-		dbfile_dir = &mem[80];
-		tpch_dir = &mem[160];
-		char line[80];
-		fgets (line, 80, fp);
-		sscanf (line, "%s\n", catalog_path);
-		fgets (line, 80, fp);
-		sscanf (line, "%s\n", dbfile_dir);
-		fgets (line, 80, fp);
-		sscanf (line, "%s\n", tpch_dir);
-		fclose (fp);
-		if (! (catalog_path && dbfile_dir && tpch_dir)) {
-			cerr << " Test settings file 'test.cat' not in correct format.\n";
-			free (mem);
-			exit (1);
-		}
-	}
-	else {
-		cerr << " Test settings files 'test.cat' missing \n";
-		exit (1);
-	}
+void setup (char *catalog_path, char *dbfile_dir, char *tpch_dir) {
 	cout << " \n** IMPORTANT: MAKE SURE THE INFORMATION BELOW IS CORRECT **\n";
 	cout << " catalog location: \t" << catalog_path << endl;
 	cout << " tpch files dir: \t" << tpch_dir << endl;
@@ -194,8 +63,8 @@ void setup () {
 	cout << " \n\n";
 
 	s = new relation (supplier, new Schema (catalog_path, supplier), dbfile_dir);
-	p = new relation (part, new Schema (catalog_path, part), dbfile_dir);
 	ps = new relation (partsupp, new Schema (catalog_path, partsupp), dbfile_dir);
+	p = new relation (part, new Schema (catalog_path, part), dbfile_dir);
 	n = new relation (nation, new Schema (catalog_path, nation), dbfile_dir);
 	li = new relation (lineitem, new Schema (catalog_path, lineitem), dbfile_dir);
 	r = new relation (region, new Schema (catalog_path, region), dbfile_dir);
@@ -205,7 +74,6 @@ void setup () {
 
 void cleanup () {
 	delete s, p, ps, n, li, r, o, c;
-	free (catalog_path);
 }
 
 #endif
