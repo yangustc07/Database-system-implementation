15633f4d1914731d7e62758fed0ce863cd5e15e3
Yang <yang@yang-cs.com>
2013-03-31 12:16:21 -0400

    Fixed a bulkloading bug.
    I fixed a bug related tu bulkloading. The last page is written
    twice, once in load(), and another in close(). This results in
    erroneous results performing relational operations.

--- src/DBFile.cc
+++ src/DBFile.cc
@@ -81,7 +81,7 @@ void DBFileBase::Load (Schema& myschema, char* loadpath) {
   Record next;
   curPage.EmptyItOut();  // creates the first page
   while (next.SuckNextRecord(&myschema, ifp)) Add(next);
-  theFile.addPage(&curPage);  // writes the last page
+  // theFile.addPage(&curPage);  // writes the last page  -- added in close()
 }
 
 void DBFileBase::MoveFirst() {
